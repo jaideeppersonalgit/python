7e4b83860a6cf56570c6de4be877e8b49f8ddcdf
Jaideep Pimpale <Jaideep.Pimpale@nice.com>
2025-02-07 16:15:53 +0530

    test commit

---
+++ compate-integer.py
@@ -0,0 +1,41 @@
+'''
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of 2 to 5, print Not Weird
+If  is even and in the inclusive range of 6 to 20, print Weird
+If  is even and greater than 20, print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+'''
+
+
+
+
+
+
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    if n % 2 != 0:
+        print ("Weird")
+    elif n % 2 == 0 and 2 <= n <= 5:
+        print ("Not Weird")
+    elif n % 2 == 0 and 6 <= n <= 20:
+        print ("Weird")
+    elif n % 2 == 0 and n > 20:
+        print ("Not Weird")
